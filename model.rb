dfbb42c6f0b2b941f717d7ba840b19f4d5f44aff
Oleg Startsev <room3764@gmail.com>
2022-12-31 22:15:56 +0500

    Add model

---
+++ model.rb
@@ -0,0 +1,9 @@
+load 'couch.rb'
+require 'json'
+module Model
+  def self.find(word)
+    response = Couch['taglex']['words'].find word
+    data = JSON.parse(response[0])
+    data.filter { |key, value| key[0] != '_' }
+  end
+end
