de5a48aeb02be0819409ad02288babe1a3022e82
Oleg Startsev <room3764@gmail.com>
2023-01-04 23:44:16 +0500

    Get several cards if any

--- model.rb
+++ model.rb
@@ -2,8 +2,10 @@ load 'couch/couch.rb'
 require 'json'
 module Model
   def self.find(word)
-    response = Couch['taglex']['words'].find word
-    data = JSON.parse(response[0])
-    data.filter { |key, value| key[0] != '_' }
+    responses = Couch['taglex']['words'].find word
+    data = responses.map { |response| JSON.parse response }
+    data.map do |card|
+      card.filter { |key, value| key[0] != '_' }
+    end
   end
 end
