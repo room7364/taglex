faed7496dbf5bce2888cb87efd74a01205d0a74f
Oleg Startsev <room3764@gmail.com>
2023-01-07 23:59:10 +0500

    Add tag field in cards

--- server.rb
+++ server.rb
@@ -1,10 +1,12 @@
 require 'webrick'
+require 'csv'
 load 'model.rb'
 
 server = WEBrick::HTTPServer.new Port: 8080, DocumentRoot: '/home/oleg/taglex'
 WEBrick::HTTPUtils::DefaultMimeTypes.store('rhtml', 'text/html')
 server.mount '/', WEBrick::HTTPServlet::FileHandler, '/home/oleg/taglex'
 server.mount_proc '/refresh' do |request, response|
+  tags = CSV.parse(request.query['tags'].force_encoding 'utf-8').flatten!.map { |line| line.strip }
   data = {
     face: {
       questions: request.query['face_questions'].force_encoding('utf-8').split("\n\n"),
@@ -13,7 +15,8 @@ server.mount_proc '/refresh' do |request, response|
     back: {
       answers: request.query['back_answers'].force_encoding('utf-8').split("\n\n"),
       examples: request.query['back_examples'].force_encoding('utf-8').split("\n\n")
-    }
+    },
+    tags: tags
   }
   id = request.query['id']
   Model.refresh id: id, data: data
