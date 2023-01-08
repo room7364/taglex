1ca12086e402e1b73e914a4b8a913992e91607f6
Oleg Startsev <room3764@gmail.com>
2023-01-08 13:39:09 +0500

    Adjust pages dispaying

--- server.rb
+++ server.rb
@@ -6,15 +6,15 @@ server = WEBrick::HTTPServer.new Port: 8080, DocumentRoot: '/home/oleg/taglex'
 WEBrick::HTTPUtils::DefaultMimeTypes.store('rhtml', 'text/html')
 server.mount '/', WEBrick::HTTPServlet::FileHandler, '/home/oleg/taglex'
 server.mount_proc '/refresh' do |request, response|
-  tags = CSV.parse(request.query['tags'].force_encoding 'utf-8').flatten!.map { |line| line.strip }
+  tags = CSV.parse(request.query['tags'].force_encoding('utf-8').strip).flatten!.map { |line| line.strip }
   data = {
     face: {
-      questions: request.query['face_questions'].force_encoding('utf-8').split("\n\n"),
-      examples: request.query['face_examples'].force_encoding('utf-8').split("\n\n")
+      questions: request.query['face_questions'].force_encoding('utf-8').strip.split("\n\n").map { |line| line.strip },
+      examples: request.query['face_examples'].force_encoding('utf-8').strip.split("\n\n").map { |line| line.strip }
     },
     back: {
-      answers: request.query['back_answers'].force_encoding('utf-8').split("\n\n"),
-      examples: request.query['back_examples'].force_encoding('utf-8').split("\n\n")
+      answers: request.query['back_answers'].force_encoding('utf-8').strip.split("\n\n").map { |line| line.strip },
+      examples: request.query['back_examples'].force_encoding('utf-8').strip.split("\n\n").map { |line| line.strip }
     },
     tags: tags
   }
